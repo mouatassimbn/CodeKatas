e6276421fde3ae268094c63b90abb11ab863d448
Mouatassim Boulahsen <boulahsen.mouatassim@gmail.com>
2022-01-24 11:16:52 -0500

    Completed even or odd

---
+++ EvenOrOdd/EvenOrOddTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOddTests
+{
+    [TestFixture]
+    internal class EvenOrOddTests
+    {
+        [Test]
+        public void EvenOrOdd_When_2_Argument_Returns_Even()
+        {
+            Assert.AreEqual("Even", EvenOrOdd.Program.EvenOrOdd(2));
+        }
+
+        [Test]
+        public void EvenOrOdd_When_1_Argument_Returns_Odd()
+        {
+            Assert.AreEqual("Odd", EvenOrOdd.Program.EvenOrOdd(1));
+        }
+
+        [Test]
+        public void EvenOrOdd_When_0_Argument_Returns_Even()
+        {
+            Assert.AreEqual("Even", EvenOrOdd.Program.EvenOrOdd(0));
+        }
+
+        [Test]
+        public void EvenOrAdd_When_7_Argument_Returns_Odd()
+        {
+            Assert.AreEqual("Odd", EvenOrOdd.Program.EvenOrOdd(7));
+        }
+
+        [Test]
+        public void EvenOrAdd_When_Negative1_Argument_Returns_Odd()
+        {
+            Assert.AreEqual("Odd", EvenOrOdd.Program.EvenOrOdd(-1));
+        }
+    }
+}
---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /**
+         * Problem:
+         * Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+         */
+        public static string EvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "Even";
+            }
+            else
+            {
+                return "Odd";
+            }
+        }
+    }
+}

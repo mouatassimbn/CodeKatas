05cf50e981401037e03a421a68fd3a6396e8f366
Mouatassim Boulahsen <boulahsen.mouatassim@gmail.com>
2022-01-24 10:35:55 -0500

    Completed Vowel count

---
+++ VowelCount/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VowelCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"abracadabra has {GetVowelCount("abracadabra")} vowels");
+            Console.WriteLine($"abracadabra has {GetVowelCount("")} vowels");
+            Console.WriteLine($"null has {GetVowelCount(null)} vowels");
+        }
+
+        /**
+         * Problem: 
+         *  Return the number (count) of vowels in the given string.
+         *  We will consider a, e, i, o, u as vowels for this Kata (but not y).
+         *  The input string will only consist of lower case letters and/or spaces.
+         */
+        public static int GetVowelCount(string str)
+        {
+            // Logic
+            // Step 2: Loop through string
+            // Step 3: Check if current characters is a vowel
+            // Step 4: If true, increase vowel count. If not continue.
+
+            int vowelCount = 0;
+
+            if (str != null)
+            {
+                foreach (char character in str)
+                {
+                    switch (character)
+                    {
+                        case 'a':
+                        case 'e':
+                        case 'i':
+                        case 'o':
+                        case 'u':
+                            vowelCount++;
+                            break;
+                        default:
+                            continue;
+                    }
+                }
+            }
+
+            return vowelCount;
+        }
+    }
+}
---
+++ VowelCount/VowelCountTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelCountTests
+{
+    [TestFixture]
+    public class VowelCountTests
+    {
+        [Test]
+        public void GetVowelCount_When_Null_Argument_Returns_Zero()
+        {
+            Assert.AreEqual(0, VowelCount.Program.GetVowelCount(null));
+        }
+
+        [Test]
+        public void GetVowelCount_When_Empty_String_Argument_Returns_Zero()
+        {
+            Assert.AreEqual(0, VowelCount.Program.GetVowelCount(" "));
+        }
+
+        [Test]
+        public void GetVowelCount_When_StringWithVowels_Argument_Returns_Count()
+        {
+            string str = "abracadabra";
+
+            Assert.AreEqual(5, VowelCount.Program.GetVowelCount(str));
+        }
+
+        [Test]
+        public void GetVowelCount_When_StringWithNoVowels_Argument_Returns_Zero()
+        {
+            string str = "crwth";
+
+            Assert.AreEqual(0, VowelCount.Program.GetVowelCount(str));
+        }
+    }
+}
